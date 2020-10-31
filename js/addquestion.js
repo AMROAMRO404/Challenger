84853888d34c71c9bb84db5a39ed534662e0b3e1
AMROAMRO404 <amro.amro1999@gmail.com>
2020-10-31 09:23:38 +0200

    put the questions part in array of question object

--- js/addquestion.js
+++ js/addquestion.js
@@ -2,12 +2,13 @@ let retrievedData = localStorage.getItem("topics");
 let topicsFromLocal = JSON.parse(retrievedData);
 document.getElementById("topic").innerHTML = "";
 for (let i = 0; i < topicsFromLocal.length; i++) {
-    document.getElementById("topic").innerHTML += "<option>" + topicsFromLocal[i] + "</option>";
+    document.getElementById("topic").innerHTML += "<option value= " + topicsFromLocal[i] + " >" + topicsFromLocal[i] + "</option>";
 }
 let i = 0;
-let questions = [];
+let questionsTitle = [];
 let arrayOfOption = [];
 let optionsArray = [];
+let questions = [];
 
 function fillOptionsToGother() {
     let options = document.getElementById('option');
@@ -27,11 +28,18 @@ function saveQuestion() {
 
     // Check for LocalStorage support.
     if (localStorage) {
-        let question = document.getElementById('question').value;
-        questions.push(question);
+        let questionTitle = document.getElementById('question-title').value;
+        let selectedTopic = document.getElementById("topic").value;
+        let arrayHoldOptions = fillOptionsToGother();
+        let questionObject = {
+            "title": questionTitle,
+            "topic": selectedTopic,
+            "options": arrayHoldOptions
+        };
+        questions.push(questionObject);
         //store array into localstorage
         localStorage.setItem("questions", JSON.stringify(questions));
-        let arrayHoldOptions = fillOptionsToGother();
+        localStorage.setItem("questionsTitle", JSON.stringify(questionsTitle));
         optionsArray.push(arrayHoldOptions);
         arrayOfOption = [];
         localStorage.setItem('optionsArray', JSON.stringify(optionsArray));
@@ -40,9 +48,10 @@ function saveQuestion() {
 
     //reinitialize the array
     arrayOfOptions = [];
-    document.getElementById('question').value = '';
-    console.log(localStorage.getItem('questions'));
+    document.getElementById('question-title').value = '';
+    console.log(localStorage.getItem('questionsTitle'));
     console.log(localStorage.getItem('arrayOfOption'));
     console.log(localStorage.getItem('optionsArray'));
     console.log(localStorage.getItem('topics'));
+    console.log(questions);
 }
--- js/questionslist.js
+++ js/questionslist.js
@@ -1,14 +1,12 @@
 function showData() {
-    let retrievedData = localStorage.getItem("topics");
-    let topicsFromLocal = JSON.parse(retrievedData);
     let retrievedData2 = localStorage.getItem("questions");
-    let questionFromLocal = JSON.parse(retrievedData2);
-    let retrievedData3 = localStorage.getItem("optionsArray");
-    let optionsArrayFromLocal = JSON.parse(retrievedData3);
+    let questionsFromLocal = JSON.parse(retrievedData2);
+
     document.getElementById("display").innerHTML = "<tr>" + "<th>" + "Question description" + "</th>" + "<th>" + "Topic Name" + "</th>" + "<th>" + "Options" + "</th>" + "<tr>";
-    for (let i = 0; i < questionFromLocal.length; i++) {
-        document.getElementById("display").innerHTML += "<tr>" + "<td>" + questionFromLocal[i] + "</td>" + "<td>" + topicsFromLocal[i] + "</td>" + "<td>" + optionsArrayFromLocal[i] + "</td>" + "</tr>";
+    for (let i = 0; i < questionsFromLocal.length; i++) {
+        document.getElementById("display").innerHTML += "<tr>" + "<td>" + questionsFromLocal[i].title + "</td>" + "<td>" + questionsFromLocal[i].topic + "</td>" + "<td>" + questionsFromLocal[i].options + "</td>" + "</tr>";
     }
+    console.log(questionsFromLocal);
 }
 
 showData();
