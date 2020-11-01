ebe278cf3342fbe992931b1cc0470b7c3af5d790
AMROAMRO404 <amro.amro1999@gmail.com>
2020-11-01 17:21:51 +0200

    set id to the topics button

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,8 @@
 let retrievedData = localStorage.getItem("topics");
 let topicsFromLocal = JSON.parse(retrievedData);
 document.getElementById("subjects").innerHTML = "";
+let setId = "topic"; //to give id for each button 
+
 for (let i = 0; i < topicsFromLocal.length; i++) {
-    document.getElementById("subjects").innerHTML += "<button   value= " + topicsFromLocal[i] + " >" + topicsFromLocal[i] + "</button>";
+    document.getElementById("subjects").innerHTML += "<button id=" + setId + (i + 1) + "  value= " + topicsFromLocal[i] + " >" + topicsFromLocal[i] + "</button>";
 }
